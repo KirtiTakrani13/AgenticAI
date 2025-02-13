a08bb3d172f197a13fc39241f1e86a36f50c3137
kirti <kirtitakrani0988@gmail.com>
2025-02-12 23:38:52 -0500

    feat: AgentiAI

---
+++ playground.py
@@ -0,0 +1,48 @@
+from phi.agent import Agent
+import phi.api
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+import openai
+
+import os
+from dotenv import load_dotenv
+import phi
+from phi.playground import Playground, serve_playground_app
+# Load environment variables from .env file
+load_dotenv()
+
+phi.api=os.getenv("PHI_API_KEY")
+
+## web search agent
+web_search_agent=Agent(
+    name="Web Search Agent",
+    role="Search the web for the information",
+    model=Groq(id="llama-3.3-70b-versatile	"),
+    tools=[DuckDuckGo()],
+    instructions=["Alway include sources"],
+    show_tool_calls=True,
+    markdown=True,
+
+)
+
+## Financial agent
+finance_agent=Agent(
+    name="Finance AI Agent",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[
+        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
+                      company_news=True),
+    ],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+
+)
+
+app=Playground(agents=[finance_agent,web_search_agent]).get_app()
+
+if __name__=="__main__":
+    serve_playground_app("playground:app",reload=True)
+
+ 
